8dbee7098cc631a882acb5d605609f3d299fe474
clemahieu <clemahieu@gmail.com>
2018-04-01 03:42:38 -0500

    Make mdb_val understand pending_info instead of the other way around.

--- rai/node/utility.hpp
+++ rai/node/utility.hpp
@@ -111,6 +111,7 @@ public:
 };
 
 class account_info;
+class pending_info;
 /**
  * Encapsulates MDB_val and provides uint256_union conversion of the data.
  */
@@ -123,6 +124,7 @@ public:
 	mdb_val (rai::uint128_union const &);
 	mdb_val (rai::uint256_union const &);
 	mdb_val (rai::account_info const &);
+	mdb_val (rai::pending_info const &);
 	void * data () const;
 	size_t size () const;
 	explicit operator rai::uint256_union () const;
